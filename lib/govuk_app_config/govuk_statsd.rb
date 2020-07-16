5c91742067974273f5f0c1597a202406f9c66273
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-07-16 11:09:39 +0100

    Permit apps to communicate with Statsd over TCP
    This is useful since Statsd running in Fargate listens on a TCP port,
    not over UDP (which is the default and what we have running in
    production).

--- lib/govuk_app_config/govuk_statsd.rb
+++ lib/govuk_app_config/govuk_statsd.rb
@@ -8,7 +8,7 @@ module GovukStatsd
 
   def self.client
     @statsd_client ||= begin
-      statsd_client = ::Statsd.new("localhost")
+      statsd_client = ::Statsd.new(ENV["STATSD_HOST"] || "localhost", 8125, ENV["STATSD_PROTOCOL"]&.to_sym || :udp)
       statsd_client.namespace = ENV["GOVUK_STATSD_PREFIX"].to_s
       statsd_client
     end
