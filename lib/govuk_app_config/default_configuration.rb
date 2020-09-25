1d51efa681a5156f079ecc1a97a7cd3726874c1a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-09-25 09:58:22 +0100

    Convert DefaultConfiguration to use Delegator pattern
    Switching to the Delegator pattern gives us more control over
    any calls to `Raven.configure`, enabling us to a) more easily
    test the expected behaviour (less reliance on mocks), and b)
    in future, allowing us to merge app-specific configuration
    with the default configuration in govuk_app_config.
    
    We anticipate using this to allow apps to specify
    `should_capture` logic without overwriting the default logic
    in govuk_app_config.
    
    NB: switching to the Delegator pattern seems to have messed up
    some of the Rails env tests in default_configuration_spec.rb.
    Details in the comments. Hope to follow this up in another commit
    later.

--- lib/govuk_app_config/default_configuration.rb
+++ lib/govuk_app_config/default_configuration.rb
@@ -1,16 +1,22 @@
 require "govuk_app_config/govuk_data_sync"
 
-class DefaultConfiguration
-  def initialize(config)
-    config.before_send = proc { |e|
+class DefaultConfiguration < SimpleDelegator
+  attr_reader :data_sync
+
+  def initialize(_raven_configuration_instance)
+    super
+
+    self.current_environment = ENV["SENTRY_CURRENT_ENV"]
+
+    self.before_send = proc { |e|
       GovukStatsd.increment("errors_occurred")
       GovukStatsd.increment("error_types.#{e.class.name.demodulize.underscore}")
       e
     }
 
-    config.silence_ready = !Rails.env.production? if defined?(Rails)
+    self.silence_ready = !Rails.env.production? if defined?(Rails)
 
-    config.excluded_exceptions = [
+    self.excluded_exceptions = [
       # Default ActionDispatch rescue responses
       "ActionController::RoutingError",
       "AbstractController::ActionNotFound",
@@ -44,15 +50,14 @@ class DefaultConfiguration
     # This will exclude exceptions that are triggered by one of the ignored
     # exceptions. For example, when any exception occurs in a template,
     # Rails will raise a ActionView::Template::Error, instead of the original error.
-    config.inspect_exception_causes_for_exclusion = true
+    self.inspect_exception_causes_for_exclusion = true
 
-    config.transport_failure_callback = proc {
+    self.transport_failure_callback = proc {
       GovukStatsd.increment("error_reports_failed")
     }
 
-    data_sync = GovukDataSync.new(ENV["GOVUK_DATA_SYNC_PERIOD"])
-
-    config.should_capture = lambda do |error_or_event|
+    @data_sync = GovukDataSync.new(ENV["GOVUK_DATA_SYNC_PERIOD"])
+    self.should_capture = lambda do |error_or_event|
       data_sync_ignored_error = error_or_event.is_a?(PG::Error) ||
         (error_or_event.respond_to?(:cause) && error_or_event.cause.is_a?(PG::Error))
 
